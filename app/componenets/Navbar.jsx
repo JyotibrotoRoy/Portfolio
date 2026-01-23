564d64d2de283fdcfa9086e798de213b9ba4cab4
jyotibrotoRoy <144539363+JyotibrotoRoy@users.noreply.github.com>
2026-01-24 01:32:56 +0530

    fixed the naming error in the nav bar

--- app/componenets/Navbar.jsx
+++ app/componenets/Navbar.jsx
@@ -73,7 +73,7 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
             <li><a className='font-Ovo' onClick={closeMenu} href="#top">Home</a></li>
             <li><a className='font-Ovo' onClick={closeMenu} href="#about">About me</a></li>
             <li><a className='font-Ovo' onClick={closeMenu} href="#services">Services</a></li>
-            <li><a className='font-Ovo' onClick={closeMenu} href="#work">Services</a></li>
+            <li><a className='font-Ovo' onClick={closeMenu} href="#work">Works</a></li>
             <li><a className='font-Ovo' onClick={closeMenu} href="#contact">Contact me</a></li>
         </ul>
         
