82cf979e5a1dad539474844d0d41b0287b6d3e77
jyotibrotoRoy <144539363+JyotibrotoRoy@users.noreply.github.com>
2026-06-14 23:20:16 +0530

    Update header and add new resume

--- app/componenets/Header.jsx
+++ app/componenets/Header.jsx
@@ -74,7 +74,7 @@ const Header = ({isDarkMode, setIsDarkMode}) => {
               initial={{ y: 30, opacity: 0 }}
               whileInView={{ y: 0, opacity:1}}
               transition={{duration: 0.6, delay: 1.2}}
-            href="/sample-resume.pdf" download className='px-10 py-3 border rounded-full
+            href="/JyotibrotoRoyLaskarResume.pdf" download className='px-10 py-3 border rounded-full
             border-gray-500 flex items-center gap-2'>
                 My resume 
             <Image src={isDarkMode ? assets.download_icon_white : assets.download_icon} alt='' className='w-4'/>
